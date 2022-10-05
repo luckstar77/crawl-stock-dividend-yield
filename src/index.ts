32da3722a339dea5580984f4a7f6303653d387de
luckstar77 <luckstar77y@gmail.com>
2022-10-05 16:33:06 +0800

    add .eslintrc.json

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { connect as redisConnect } from './db/redis';
 import { connect as mongodbConnect } from './db/mongodb';
 import axios from 'axios';
-import * as acorn from "acorn";
-import * as cheerio from "cheerio";
-import * as R from 'ramda'
+import * as acorn from 'acorn';
+import * as cheerio from 'cheerio';
+import * as R from 'ramda';
 
 const COLLECTION = 'stock';
 const STOCK_IDS_URL = 'https://goodinfo.tw/tw/StockLib/js/TW_STOCK_ID_NM_LIST.js?0';
@@ -29,11 +29,12 @@ interface Dividend {
     const stockIdsParsed = acorn.parse(
         stockIdsText,
         { ecmaVersion: 2020 }
-      );
+    );
 
-       // TODO: https://github.com/acornjs/acorn/issues/741
-       // @ts-ignore
-    const stockIds: {value: string}[] = stockIdsParsed.body[0].declarations[0].init.elements
+    // TODO: https://github.com/acornjs/acorn/issues/741
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const stockIds: {value: string}[] = stockIdsParsed.body[0].declarations[0].init.elements;
 
     // stockIds the first useful value is third element.
     const STOCK_IDS_MAX = stockIds.length;
@@ -41,30 +42,31 @@ interface Dividend {
     if(stockIdsIndex < 2) stockIdsIndex = 2;
     const [stockId, stockName] = stockIds[stockIdsIndex].value.split(' ');
 
-       // TODO: https://github.com/acornjs/acorn/issues/741
-       // @ts-ignore
+    // TODO: https://github.com/acornjs/acorn/issues/741
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     const {data: dividendText} = await axios.get(DIVIDEND_PREFIX_URL + stockId);
-    let $ = cheerio.load(dividendText);
+    const $ = cheerio.load(dividendText);
     const price = parseFloat($('body > table:nth-child(8) > tbody > tr > td:nth-child(3) > table:nth-child(1) > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(3) > td:nth-child(1)').text());
-    const allAvgCashYields = parseFloat($('#divDividendSumInfo > div > div > table > tbody > tr:nth-child(4) > td:nth-child(5)').text())
-    const allAvgRetroactiveYields = parseFloat($('#divDividendSumInfo > div > div > table > tbody > tr:nth-child(6) > td:nth-child(5)').text())
+    const allAvgCashYields = parseFloat($('#divDividendSumInfo > div > div > table > tbody > tr:nth-child(4) > td:nth-child(5)').text());
+    const allAvgRetroactiveYields = parseFloat($('#divDividendSumInfo > div > div > table > tbody > tr:nth-child(6) > td:nth-child(5)').text());
 
     let yearText:string;
     let year:number;
-    let $trs = $('#tblDetail > tbody > tr');
-    let dividends: Dividend = {};
+    const $trs = $('#tblDetail > tbody > tr');
+    const dividends: Dividend = {};
     for(let i = 4; i < $trs.length - 1; i++) {
-        let dividendState: DividendState = DividendState.NOTHING 
-        let $dividendTr = $trs.eq(i);
-        yearText = $dividendTr.children('td').eq(0).text()
+        let dividendState: DividendState = DividendState.NOTHING; 
+        const $dividendTr = $trs.eq(i);
+        yearText = $dividendTr.children('td').eq(0).text();
         if(!isNaN(yearText as any)) {
             year = parseInt(yearText);
             dividends[year] = [];
         } else if(yearText !== '∟') continue;
-        const cashDividendText = $dividendTr.children('td').eq(3).text()
-        const stockDividendText = $dividendTr.children('td').eq(6).text()
-        const cashDividendSpendDaysText = $dividendTr.children('td').eq(10).text()
-        const stockDividendSpendDaysText = $dividendTr.children('td').eq(11).text()
+        const cashDividendText = $dividendTr.children('td').eq(3).text();
+        const stockDividendText = $dividendTr.children('td').eq(6).text();
+        const cashDividendSpendDaysText = $dividendTr.children('td').eq(10).text();
+        const stockDividendSpendDaysText = $dividendTr.children('td').eq(11).text();
         if(cashDividendText !== '0' && cashDividendText !== '-') {
             if(cashDividendSpendDaysText !== '-') dividendState = DividendState.SUCCESS;
             else dividendState = DividendState.FAILURE;
@@ -84,9 +86,9 @@ interface Dividend {
             else return false;
         }
         
-       return R.any(R.equals(1))( R.splitAt(1, value)[1]);
+        return R.any(R.equals(1))( R.splitAt(1, value)[1]);
         
-    }, dividends)
+    }, dividends);
     const dividendsFailures = R.keys(dividendsFailureObject);
     const successRate = (1-(dividendsFailures.length / dividendsValues.length))*100.00;
 
@@ -102,7 +104,9 @@ interface Dividend {
         $currentDate: { updated: true },
     }, {
         upsert: true,
-    })
+    });
     
     await redisClient.incr('STOCK_ID_INDEX');
+
+    process.exit();
 })();
